2d65eda2ed8af7a6792387d57014a6ca9f0f08bf
Stephen FitzSimon <stephen.fitzsimon@gmail.com>
2022-07-20 12:02:02 -0500

    added model ex

--- prepare.py
+++ prepare.py
@@ -66,6 +66,10 @@ def lemmatize(s):
     wnl = nltk.stem.WordNetLemmatizer()
     return [wnl.lemmatize(word) for word in s]
 
+def lemmatize_split(s):
+    wnl = nltk.stem.WordNetLemmatizer()
+    return ' '.join([wnl.lemmatize(word) for word in s.split()])
+
 def mass_lemmatize(content_list):
     for content in content_list:
         content['lemmatized'] = lemmatize(content['clean'])
@@ -76,10 +80,24 @@ def remove_stopwords(s, extra_words = ['codeup'], exclude_words = ['shan']):
     stopwords_list = stopwords_list + extra_words
     return [word for word in s if word not in stopwords_list]
 
+def remove_stopwords_split(s):
+    stopwords_list = nltk.corpus.stopwords.words('english')
+    return ' '.join([word for word in s.split() if word not in stopwords_list])
+
 def mass_remove_stopwords(content_list):
     for content in content_list:
         content['clean'] = remove_stopwords(content['clean'])
     return content_list
+
+def make_dataframe_news(text_dict):
+    df = pd.DataFrame(text_dict)
+    df['clean'] = df['content'].str.lower()
+    df['clean'] = df['clean'].str.replace(r"[^a-zA-Z0-9\s]", "")
+    df['clean'] = df['clean'].apply(lambda r : unicodedata.normalize('NFKD', r).encode('ascii', 'ignore').decode('utf-8','ignore'))
+    df['clean'] = df['clean'].apply(remove_stopwords_split)
+    wnl = nltk.stem.WordNetLemmatizer()
+    df['lemmatized'] = df['clean'].apply(lemmatize_split)
+    return df
 
 def make_dataframe(text_dict):
     text_dict = basic_clean(text_dict)
